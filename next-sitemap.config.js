c3a17e771e3ef3c93114e87e904b7bdce9990ae1
Akshay <akshay10sangani@gmail.com>
2025-10-11 12:43:33 +0530

    sitemap issue fixed

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://www.quickjson.net",
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: "daily",
-    priority: 0.7,
-  }
-  
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,6 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const urls = [
     `${baseUrl}/`,
     `${baseUrl}/json-formatter`,
+    `${baseUrl}/tools`,
     // Add other pages here
   ]
 
