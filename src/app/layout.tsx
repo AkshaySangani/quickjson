18a3cf7e85aaa8f0f51ed918f2dcdb054c07cfbe
Akshay <akshay10sangani@gmail.com>
2025-11-11 18:13:03 +0530

    new way cheking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <AdSense pubId="1776553000817606" />
+      <meta name="google-adsense-account" content="ca-pub-1776553000817606" />
       </head>
       <body className="bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100 transition-colors duration-300">
         <Providers>
